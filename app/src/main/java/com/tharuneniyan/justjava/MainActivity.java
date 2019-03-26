51327bda6fd92d5a80be122c0e24efc527032d9f
tharun <tharuneniyan4@gmail.com>
2019-03-26 20:35:30 +0530

    JustJava Code in OOP2

--- app/src/main/java/com/tharuneniyan/justjava/MainActivity.java
+++ app/src/main/java/com/tharuneniyan/justjava/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -14,21 +16,38 @@ import java.text.NumberFormat;
 public class MainActivity extends AppCompatActivity {
     int quantity = 2;
     TextView price_text_view ;
+    CheckBox checkboxWhippedCream,checkboxChocolate;
+    boolean isCheckboxCheckedWhippedCream = false,isCheckboxCheckedChocolate = false;
+    EditText nameEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         price_text_view = findViewById(R.id.price_text_view);
+        checkboxWhippedCream = findViewById(R.id.checkboxWhippedCream);
+        checkboxChocolate = findViewById(R.id.checkboxChocolate);
+        nameEditText = findViewById(R.id.nameEditText);
+
     }
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price;
-        priceMessage = priceMessage + "\nThank you!";
+        int price = calculatePrice();
+        isCheckboxCheckedWhippedCream = checkboxWhippedCream.isChecked();
+        isCheckboxCheckedChocolate = checkboxChocolate.isChecked();
+        String userName = nameEditText.getText().toString();
+        String priceMessage = createOrderSummary(price,isCheckboxCheckedWhippedCream,isCheckboxCheckedChocolate,userName);
         displayMessage(priceMessage);
+        
+    }
+
+    private String createOrderSummary(int price,boolean isCheckboxChecked,boolean isCheckboxCheckedChocolate,String userName) {
+        String priceMessage = "Name: "+userName+"\nAdd whipped cream?"+isCheckboxChecked+"\nAdd Chocolate?"+isCheckboxCheckedChocolate+"\nQuantity: "+quantity+"\nTotal: $" + price;
+        priceMessage = priceMessage + "\nThank you!";
+        return priceMessage;
     }
 
     private void displayMessage(String message) {
@@ -53,9 +72,9 @@ public class MainActivity extends AppCompatActivity {
     /**
      * This method displays the given quantity value on the screen.
      */
-    private void displayQuantity(int number) {
+    private void displayQuantity(int numberOfCoffees) {
         TextView quantityTextView =  findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+        quantityTextView.setText("" + numberOfCoffees);
     }
 
     private void displayPrice(int number) {
@@ -63,5 +82,8 @@ public class MainActivity extends AppCompatActivity {
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
-
+    private int calculatePrice() {
+        int price = quantity * 5;
+        return price;
+    }
 }
