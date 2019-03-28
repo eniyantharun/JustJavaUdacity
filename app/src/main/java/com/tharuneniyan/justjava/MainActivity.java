76e290066bdd6f31207946c6d54424736b97347c
tharun <tharuneniyan4@gmail.com>
2019-03-28 17:16:01 +0530

    JustJava Code in OOP2 final after user input

--- app/src/main/java/com/tharuneniyan/justjava/MainActivity.java
+++ app/src/main/java/com/tharuneniyan/justjava/MainActivity.java
@@ -1,5 +1,7 @@
 package com.tharuneniyan.justjava;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,6 +9,7 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -35,17 +38,26 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        int price = calculatePrice();
         isCheckboxCheckedWhippedCream = checkboxWhippedCream.isChecked();
         isCheckboxCheckedChocolate = checkboxChocolate.isChecked();
+        int price = calculatePrice(isCheckboxCheckedWhippedCream, isCheckboxCheckedChocolate);
         String userName = nameEditText.getText().toString();
         String priceMessage = createOrderSummary(price,isCheckboxCheckedWhippedCream,isCheckboxCheckedChocolate,userName);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+//        intent.putExtra(Intent.EXTRA_EMAIL, addresses); /to field of the email
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just Java order for "+userName);
+        intent.putExtra(Intent.EXTRA_TEXT,priceMessage);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
         displayMessage(priceMessage);
-        
+
     }
 
-    private String createOrderSummary(int price,boolean isCheckboxChecked,boolean isCheckboxCheckedChocolate,String userName) {
-        String priceMessage = "Name: "+userName+"\nAdd whipped cream?"+isCheckboxChecked+"\nAdd Chocolate?"+isCheckboxCheckedChocolate+"\nQuantity: "+quantity+"\nTotal: $" + price;
+    private String createOrderSummary(int price,boolean isCheckboxCheckedWhippedCream,boolean isCheckboxCheckedChocolate,String userName) {
+        String priceMessage = "Name: "+userName+"\nAdd whipped cream?"+isCheckboxCheckedWhippedCream+"\nAdd Chocolate?"+isCheckboxCheckedChocolate+"\nQuantity: "+quantity+"\nTotal: $" + price;
         priceMessage = priceMessage + "\nThank you!";
         return priceMessage;
     }
@@ -56,9 +68,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void increment(View view){
-        quantity += 1;
-        displayQuantity(quantity);
-        displayPrice(quantity*5);
+        if(quantity<=100) {
+            quantity += 1;
+            displayQuantity(quantity);
+            displayPrice(quantity * 5);
+        }
+        else
+            Toast.makeText(this, "100 is the max coffee", Toast.LENGTH_SHORT).show();
     }
 
     public void decrement(View view){
@@ -67,6 +83,8 @@ public class MainActivity extends AppCompatActivity {
             displayQuantity(quantity);
             displayPrice(quantity * 5);
         }
+        else
+            Toast.makeText(this, "Select at least one coffee", Toast.LENGTH_SHORT).show();
     }
 
     /**
@@ -79,11 +97,17 @@ public class MainActivity extends AppCompatActivity {
 
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+                priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
-    private int calculatePrice() {
-        int price = quantity * 5;
-        return price;
+    private int calculatePrice(boolean isCheckboxCheckedWhippedCream, boolean isCheckboxCheckedChocolate) {
+        int basePrice =  5;
+        if(isCheckboxCheckedWhippedCream){
+            basePrice = basePrice + 1;
+        }
+        if(isCheckboxCheckedChocolate){
+            basePrice = basePrice + 2;
+        }
+        return basePrice * quantity;
     }
 }
